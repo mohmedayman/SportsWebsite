0c6f59c1a154a10fbe520b7fca262be3e588e11a
mohmedayman <mohmedayman1110@gmail.com>
2021-06-12 15:00:28 +0200

    final form

--- co.js
+++ co.js
@@ -1,5 +1,9 @@
 let openBtn = document.querySelector('.fa-bars');
-
+window.onload=pageLoad;
+function pageLoad(){
+	var contactForm = document.getElementById("contactForm");
+	contactForm.onsubmit= validate;
+}
 openBtn.addEventListener('click', ()=>{
     document.getElementById("nav").style.display="flex";
     document.getElementById("nav").style.flexDirection="column";
@@ -14,3 +18,41 @@ openBtn.addEventListener('click', ()=>{
     document.getElementById("nav4").style.width="100%";
     document.getElementById("nav5").style.width="100%";
     });
+  
+    function validate(){
+        var check = true;
+        var fname = document.getElementById("n").value;
+        var mail = document.getElementById("m").value;
+        var message= document.getElementById("me").value;
+    
+        if(fname == ""){
+            alert('Name is required !');
+            check= false;
+        }
+        
+        if(mail == ""){
+            alert('E-mail is required !');
+            check= false;
+        }
+    
+        if(message == ""){
+            alert('Message is required !');
+            check= false;
+        }
+        if (!fname.match(/^[A-Za-z ]+$/)){
+            alert("only letters or space allowed in name field");
+            check= false;
+        }
+        if (!mail.match(/^[a-zA-z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$/)){
+            alert("Email is Not Valid");
+            check=false;
+        }
+        if (check==true) {
+            alert("Got it! Thank you.");
+        }
+        else{
+            return check;
+        }
+    }
+    
+        
